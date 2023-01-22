3e45c69be4b4851b8166ba36aba5f407581cd0e9
armindpi <116251612+armindpi@users.noreply.github.com>
2023-01-22 14:40:05 +0600

    Add files via upload

--- themepadma/functions.php
+++ themepadma/functions.php
@@ -1,25 +1,22 @@
 <?php
 wp_enqueue_style( 'style-name', get_stylesheet_uri() );
-wp_enqueue_style( 'style-boot', get_template_directory_uri() . '/asstes/css/bootstrap.min.css');
+wp_enqueue_style( 'style-boot', get_template_directory_uri() .'/asstes/css/bootstrap.min.css' );
 wp_enqueue_script( 'script-name', get_template_directory_uri() . '/asstes/js/bootstrap.bundle.min.js', array(), '1.0.0', true );
 
 
 add_theme_support( 'title-tag' );
-add_theme_support( 'custom-logo', array(
-    'height' => 480,
-    'width'  => 720,
-) );
+add_theme_support( 'custom-logo' );
 add_theme_support( 'post-thumbnails' );
-/**
- * Add a sidebar.
- */
-function wpdocs_theme_slug_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'BD logo', 'textdomain' ),
-		'id'            => 'bdlogo',
-		'before_widget' => ' ',
-		'after_widget'  => ' ',
-		
-	) );
-}
-add_action( 'widgets_init', 'wpdocs_theme_slug_widgets_init' );
+
+
+
+register_sidebar([
+    'name'=> ' Bd Logo',
+    'id'=> 'bdlogo',
+    'before_widget'=>'',
+    'after_widget'=>'',
+    
+
+]);
+  
+
